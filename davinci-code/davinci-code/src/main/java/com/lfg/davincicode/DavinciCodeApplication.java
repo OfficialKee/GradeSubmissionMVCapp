cc206f9be649acdf5408d0ef3d8e5ee387ad93d4
Kee <shikee.maloney@codeimmersives.com>
2023-04-26 18:21:40 -0400

    all

---
+++ davinci-code/davinci-code/src/main/java/com/lfg/davincicode/DavinciCodeApplication.java
@@ -0,0 +1,13 @@
+package com.lfg.davincicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavinciCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavinciCodeApplication.class, args);
+	}
+
+}
---
+++ davinci-code/davinci-code/src/test/java/com/lfg/davincicode/DavinciCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfg.davincicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavinciCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
