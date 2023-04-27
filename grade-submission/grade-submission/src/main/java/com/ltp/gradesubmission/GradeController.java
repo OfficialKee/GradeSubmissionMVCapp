2c6d3ce6b659b194e994047ce7fb65ca672e0870
Kee <shikee.maloney@codeimmersives.com>
2023-04-26 20:27:03 -0400

    MVC

---
+++ grade-submission/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -0,0 +1,17 @@
+package com.ltp.gradesubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GradeController {
+    @GetMapping("/grades")
+    public String getGrades(){
+        return "grades";
+    }
+    @GetMapping("/shows")
+    public String getShows(){
+        return "shows";
+    }
+    
+}
